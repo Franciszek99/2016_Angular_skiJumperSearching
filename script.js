5c6ae2afe365cb1292935631cfb221a5d24be8c2
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-20 12:31:27 +0000

    Create controller and show its content
    Upload contents from .json and add it. Moreover add this controller to page

--- script.js
+++ script.js
@@ -1,4 +1,192 @@
 var app = angular.module('appSki', [])
                 .controller('contrTabSki', [
-                        $scope
-                    ]);
+                        '$scope', function($scope) {
+                            
+                        $scope.jumpers = [
+ {
+   "FIELD2": 2148,
+   "FIELD3": "AAGHEIM Morten",
+   "FIELD7": "NOR"
+ },
+ {
+   "FIELD2": 6573,
+   "FIELD3": "AALIEN Fredrik",
+   "FIELD7": "NOR"
+ },
+ {
+   "FIELD2": 5476,
+   "FIELD3": "AALST VAN DER Oldrik",
+   "FIELD7": "NED"
+ },
+ {
+   "FIELD2": 2650,
+   "FIELD3": "AALTO Asko",
+   "FIELD7": "FIN"
+ },
+ {
+   "FIELD2": 4882,
+   "FIELD3": "AARAAS Jon",
+   "FIELD7": "NOR"
+ },
+ {
+   "FIELD2": 2828,
+   "FIELD3": "AARNES Tom Age",
+   "FIELD7": "NOR"
+ },
+ {
+   "FIELD2": 6213,
+   "FIELD3": "AAS Daniel Haugane",
+   "FIELD7": "NOR"
+ },
+ {
+   "FIELD2": 6733,
+   "FIELD3": "AASBERG Nikolai",
+   "FIELD7": "NOR"
+ },
+ {
+   "FIELD2": 2213,
+   "FIELD3": "AASEN Otto",
+   "FIELD7": "NOR"
+ },
+ {
+   "FIELD2": 2768,
+   "FIELD3": "AASTROEM Magnus",
+   "FIELD7": "SWE"
+ },
+ {
+   "FIELD2": 2628,
+   "FIELD3": "ABERER Gebhard",
+   "FIELD7": "AUT"
+ },
+ {
+   "FIELD2": 5943,
+   "FIELD3": "ABERGER Marco",
+   "FIELD7": "AUT"
+ },
+ {
+   "FIELD2": 3852,
+   "FIELD3": "ABRAMOW Dimitri",
+   "FIELD7": "SOV"
+ },
+ {
+   "FIELD2": 4060,
+   "FIELD3": "ACKERMANN Helmut",
+   "FIELD7": "GER"
+ },
+ {
+   "FIELD2": 3737,
+   "FIELD3": "ACKERMANN Tim",
+   "FIELD7": "GER"
+ },
+ {
+   "FIELD2": 4413,
+   "FIELD3": "ADACHI Masahito",
+   "FIELD7": "JPN"
+ },
+ {
+   "FIELD2": 4181,
+   "FIELD3": "ADAMI Giancarlo",
+   "FIELD7": "ITA"
+ },
+ {
+   "FIELD2": 7345,
+   "FIELD3": "ADBUKHALILOV Islam",
+   "FIELD7": "KAZ"
+ },
+ {
+   "FIELD2": 3234,
+   "FIELD3": "AEYRI Riku",
+   "FIELD7": "FIN"
+ },
+ {
+   "FIELD2": 3180,
+   "FIELD3": "AFANASENKO Dimitri",
+   "FIELD7": "BLR"
+ },
+ {
+   "FIELD2": 4126,
+   "FIELD3": "AFANASJEW Viktor",
+   "FIELD7": "SOV"
+ },
+ {
+   "FIELD2": 3168,
+   "FIELD3": "AHAVA Jarkko",
+   "FIELD7": "FIN"
+ },
+ {
+   "FIELD2": 6677,
+   "FIELD3": "AHIBALAU Mikita",
+   "FIELD7": "BLR"
+ },
+ {
+   "FIELD2": 5013,
+   "FIELD3": "AHLBORN Alexandra",
+   "FIELD7": "GER"
+ },
+ {
+   "FIELD2": 2959,
+   "FIELD3": "AHLERS Ann-Kristin",
+   "FIELD7": "GER"
+ },
+ {
+   "FIELD2": 4574,
+   "FIELD3": "AHONEN Mika",
+   "FIELD7": "FIN"
+ },
+ {
+   "FIELD2": 2709,
+   "FIELD3": "AHONEN Veli-Matti",
+   "FIELD7": "FIN"
+ },
+ {
+   "FIELD2": 5746,
+   "FIELD3": "AIBA Yutaka",
+   "FIELD7": "JPN"
+ },
+ {
+   "FIELD2": 3447,
+   "FIELD3": "AIMONI Giacomo",
+   "FIELD7": "ITA"
+ },
+ {
+   "FIELD2": 5651,
+   "FIELD3": "AIRIAU Gabriel",
+   "FIELD7": "FRA"
+ },
+ {
+   "FIELD2": 5650,
+   "FIELD3": "AIRIAU Gatien",
+   "FIELD7": "FRA"
+ },
+ {
+   "FIELD2": 5318,
+   "FIELD3": "AIRIAU Gautier",
+   "FIELD7": "FRA"
+ },
+ {
+   "FIELD2": 6904,
+   "FIELD3": "AIRIAU Ghislain",
+   "FIELD7": "FRA"
+ },
+ {
+   "FIELD2": 6188,
+   "FIELD3": "AITKEN Simeon",
+   "FIELD7": "USA"
+ },
+ {
+   "FIELD2": 2582,
+   "FIELD3": "AIZAWA Hiroyasu",
+   "FIELD7": "JPN"
+ },
+ {
+   "FIELD2": 4634,
+   "FIELD3": "AJNIK Luka",
+   "FIELD7": "SLO"
+ },
+ {
+   "FIELD2": 6478,
+   "FIELD3": "AJNIK Matic",
+   "FIELD7": "SLO"
+ }
+];
+                }]);
